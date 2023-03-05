2b6e1d95b830d1e121f04e2072a913a54853c39a
GianIuca <gianluca.ferrara@ti8m.ch>
2023-03-05 16:17:56 +0100

    Initialized buttons and set Default value to Radio button in MainActivity

--- WhatsmyIQ/app/src/main/java/ch/zli/whatsmyiq/MainActivity.java
+++ WhatsmyIQ/app/src/main/java/ch/zli/whatsmyiq/MainActivity.java
@@ -3,12 +3,37 @@ package ch.zli.whatsmyiq;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+    private RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        radioGroup = findViewById(R.id.radioGroup);
+        // set default firstSelection
+        setDefault();
+
+        Button button = findViewById(R.id.submitButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int radioID = radioGroup.getCheckedRadioButtonId();
+
+                RadioButton radioButton = findViewById(radioID);
+                String option = radioButton.getText().toString();
+            }
+        });
     }
+
+    private void setDefault() {
+        radioGroup.check(R.id.firstSelection);
+    }
+
 }
