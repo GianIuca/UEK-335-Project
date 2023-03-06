64d374418410e794c2c048402226728b2052b510
GianIuca <gianluca.ferrara@ti8m.ch>
2023-03-06 22:57:50 +0100

    Merged Calculation service into Notification service

--- WhatsmyIQ/app/src/main/java/ch/zli/whatsmyiq/service/NotificationService.java
+++ WhatsmyIQ/app/src/main/java/ch/zli/whatsmyiq/service/NotificationService.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
@@ -17,15 +18,46 @@ public class NotificationService extends Service {
     private static final String CHANNEL_ID = "defaultChannel";
     private static final String CHANNEL_NAME = "Default Channel";
     private NotificationManager notificationManager;
-    private final String note = "Das hast du gut gemacht";
-    private final String note2 = "Das hast du SUPER gemacht!";
-    private final String note3 = "Das nächste Mal wirds besser";
-    private final String note4 = "Ich würde dir empfehlen, das zu wiederholen";
+
+    private int correct = 0;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
+        SharedPreferences sharedPreferences = getSharedPreferences("database", MODE_PRIVATE);
+
+        boolean question1 = sharedPreferences.getBoolean("question1", false);
+        boolean question2 = sharedPreferences.getBoolean("question2", false);
+        boolean question3 = sharedPreferences.getBoolean("question3", false);
+        boolean question4 = sharedPreferences.getBoolean("question4", false);
+
+        boolean[] questions = {question1, question2, question3, question4};
+
+        for (boolean b : questions) {
+            if (b) {
+                correct++;
+            }
+        }
+
+        int iq = 80;
+
+        if (correct == 1) {
+            iq = 95;
+        } else if (correct == 2) {
+            iq = 101;
+        } else if (correct == 3) {
+            iq = 110;
+        } else if (correct == 4) {
+            iq = 120;
+        }
+
+        sharedPreferences.edit().putInt("iq", iq).apply();
+
         sendNotification();
-        return super.onStartCommand(intent, flags, startId);
+
+        stopSelf();
+
+        return START_NOT_STICKY;
     }
 
     @Nullable
@@ -42,10 +74,15 @@ public class NotificationService extends Service {
         notificationManager.createNotificationChannel(channel);
     }
 
+    //TODO add notes
     private void sendNotification() {
+        int iq;
+        SharedPreferences sharedPreferences = getSharedPreferences("database", MODE_PRIVATE);
+        iq = sharedPreferences.getInt("iq", 69);
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(android.R.drawable.ic_media_play)
-                .setContentTitle("Dein IQ ist: ")
+                .setContentTitle("Dein IQ ist: " + iq)
                 .setContentText("Das hast du gut gemacht ")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
@@ -54,7 +91,8 @@ public class NotificationService extends Service {
         long[] pattern = {0, 187, 690, 420};
         vibrator.vibrate(pattern, -1);
 
-
         notificationManager.notify(0, builder.build());
     }
+
 }
+
