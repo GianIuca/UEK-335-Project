e67a14f6f5e44c777c766d9db8ed8b3d9b704abe
GianIuca <gianluca.ferrara@ti8m.ch>
2023-03-06 21:15:52 +0100

    Added self-made icon to the main activity

--- WhatsmyIQ/app/src/main/java/ch/zli/whatsmyiq/MainActivity.java
+++ WhatsmyIQ/app/src/main/java/ch/zli/whatsmyiq/MainActivity.java
@@ -1,21 +1,30 @@
 package ch.zli.whatsmyiq;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView appIcon;
+
     private RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        appIcon = findViewById(R.id.app_icon);
+        appIcon.setImageDrawable(getAppIcon());
 
         radioGroup = findViewById(R.id.radioGroup);
         // set default firstSelection
@@ -30,11 +39,22 @@ public class MainActivity extends AppCompatActivity {
                 RadioButton radioButton = findViewById(radioID);
                 String option = radioButton.getText().toString();
 
+                SharedPreferences sharedPreferences = getSharedPreferences("database", MODE_PRIVATE);
+                sharedPreferences.edit().putString("age", option).apply();
+
                 Intent intent = new Intent(MainActivity.this, FirstQuestionActivity.class);
-                intent.putExtra("age", radioButton.getText().toString());
                 startActivity(intent);
             }
         });
+    }
+
+    private Drawable getAppIcon() {
+        try {
+            return getPackageManager().getApplicationIcon(getPackageName());
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     private void setDefault() {
