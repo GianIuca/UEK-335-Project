12bbc94846431d7177959bd7bf0aa8ccf7778aa3
GianIuca <gianluca.ferrara@ti8m.ch>
2023-03-06 08:55:14 +0100

    Implemented second activity

--- WhatsmyIQ/app/src/main/java/ch/zli/whatsmyiq/MainActivity.java
+++ WhatsmyIQ/app/src/main/java/ch/zli/whatsmyiq/MainActivity.java
@@ -1,13 +1,13 @@
 package ch.zli.whatsmyiq;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private RadioGroup radioGroup;
--- WhatsmyIQ/app/src/main/java/ch/zli/whatsmyiq/SecondQuestionActivity.java
+++ WhatsmyIQ/app/src/main/java/ch/zli/whatsmyiq/SecondQuestionActivity.java
@@ -1,14 +1,48 @@
 package ch.zli.whatsmyiq;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class SecondQuestionActivity extends AppCompatActivity {
+    private TextView title;
+    private TextView question;
+    private EditText input;
+    private Button submitButton;
+    private boolean solvedSecond = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_question);
+
+        title = findViewById(R.id.title);
+        question = findViewById(R.id.question3);
+        input = findViewById(R.id.input2);
+        submitButton = findViewById(R.id.submitButton3);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userInput = input.getText().toString().trim();
+                String solution = "47";
+
+                if (userInput.equals(solution)) {
+                    Toast.makeText(SecondQuestionActivity.this, "Input: " + userInput + ", is true", Toast.LENGTH_SHORT).show();
+                    solvedSecond = true;
+                } else {
+                    Toast.makeText(SecondQuestionActivity.this, "Wrong, the solution is: :" + solution, Toast.LENGTH_SHORT).show();
+                    solvedSecond = false;
+                }
+                Intent intent = new Intent(SecondQuestionActivity.this, ThirdQuestionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
